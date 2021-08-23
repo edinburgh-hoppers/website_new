d45f29b971b76b6b2d8fc31878e07bd4d7066690
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-08-23 16:03:51 +0200

    carousel finished and responsive

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,15 +4,38 @@ particlesJS.load('particles-js', 'particles.json', function () {
   console.log('callback - particles.js config loaded');
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  new Splide('.splide', {
-    type: 'loop',
-    height: '9rem',
-    width: '80%',
-    autoWidth: true,
-    focus: 'center',
-    perPage: 2,
-    gap: '2em',
+// document.addEventListener('DOMContentLoaded', function () {
+//   new Splide('.splide', {
+//     type: 'loop',
+//     height: '9rem',
+//     width: '80%',
+//     autoWidth: true,
+//     focus: 'center',
+//     perPage: 2,
+//     gap: '2em',
+//     autoplay: true,
+//   }).mount();
+// });
+
+$(document).ready(() => {
+  $('.carousel-wrapper .slick').slick({
+    dots: true,
+    slidesToShow: 2,
+    centerMode: true,
+    variableWidth: true,
+    slidesToScroll: 1,
     autoplay: true,
-  }).mount();
-});
+    autoplaySpeed: 2000,
+    //adaptiveHeight: true,
+    arrows: false,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: '890',
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+})
