577b223f658f872fa4a1fd0c5e62f6603d48c155
JuliaCastillo <JuliaCastillo@users.noreply.github.com>
2021-08-23 16:33:53 +0200

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,18 +4,6 @@ particlesJS.load('particles-js', 'particles.json', function () {
   console.log('callback - particles.js config loaded');
 });
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   new Splide('.splide', {
-//     type: 'loop',
-//     height: '9rem',
-//     width: '80%',
-//     autoWidth: true,
-//     focus: 'center',
-//     perPage: 2,
-//     gap: '2em',
-//     autoplay: true,
-//   }).mount();
-// });
 
 $(document).ready(() => {
   $('.carousel-wrapper .slick').slick({
